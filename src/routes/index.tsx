dbf41ccaea41a736741322a0f563ba6a1fe96414
TelecomSistemas <daniel.alvarado.milano@gmail.com>
2023-12-26 10:28:06 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,11 +5,11 @@ export default function Home() {
   return (
     <main>
       <Title>Daniel Alvarado Desarrollo Web</Title>
-      <h1>Contacto 04144961494!</h1>
+      <h1>Daniel Alvarado Desarrollo Web!</h1>
       <Counter />
       <p>
         Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
+        <a href="https://www.linkedin.com/in/daniel-alvarado-5a14a449/" target="_blank">
           start.solidjs.com
         </a>{" "}
         to learn how to build SolidStart apps.
