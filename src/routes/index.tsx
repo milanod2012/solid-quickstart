a21ab3e040fbffee06b6ab5a0ddcae368814a4a1
TelecomSistemas <daniel.alvarado.milano@gmail.com>
2023-12-26 10:23:26 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,8 +4,8 @@ import Counter from "~/components/Counter";
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
+      <Title>Daniel Alvarado Desarrollo Web</Title>
+      <h1>Contacto 04144961494!</h1>
       <Counter />
       <p>
         Visit{" "}
